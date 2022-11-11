cbd3f0eee71e8a7551255fadf8d331c4fcf00a85
Vanessa Mercado <finessevanes@Vanessas-MacBook-Pro.local>
2022-11-11 05:59:38 -0700

    pairing works

--- src/App.js
+++ src/App.js
@@ -1,15 +1,81 @@
-import { useEffect } from "react";
-import { SignClient } from "@walletconnect/sign-client";
+import SignClient from "@walletconnect/sign-client";
+import QRCodeModal from "@walletconnect/qrcode-modal";
+import { useEffect, useState, useCallback } from "react";
 
-function App({ buttonClick }) {
-  const handleConnect = () => {
-    console.log("beep boop");
-  };
+function App() {
+  const [client, setClient] = useState();
+  const [pairing, setPairing] = useState();
+
+  const creatSignClient = useCallback(async () => {
+    try {
+      const signClient = await SignClient.init({
+        projectId: "9338b83ff89083cd0ee7da8cea475c23",
+        metadata: {
+          name: "Example Dapp",
+          description: "Example Dapp",
+          url: "#",
+          icons: ["https://walletconnect.com/walletconnect-logo.png"],
+        },
+      });
+      console.log('signClient', signClient)
+      setClient(signClient)
+    } catch (e) {
+      console.error(e);
+    }
+  })
+
+  useEffect(() => {
+    console.log('does client exist: ', client)
+    if(!client){
+      creatSignClient()
+    }
+  }, [client])
+
+
+
+  const handleConnect = useCallback(async () => {
+    try {
+      const requiredNamespaces = {
+        eip155: {
+          methods: [
+            "eth_sendTransaction",
+            "eth_signTransaction",
+            "eth_sign",
+            "personal_sign",
+            "eth_signTypedData",
+          ],
+          chains: ["eip155:1"],
+          events: ["chainChanged", "accountsChanged"],
+        },
+      };
+
+      const { uri, approval } = await client.connect({
+        pairingTopic: pairing?.topic,
+        requiredNamespaces,
+      });
+
+      // Open QRCode modal if a URI was returned (i.e. we're not connecting an existing pairing).
+      if (uri) {
+        QRCodeModal.open(uri, () => {
+          console.log("EVENT", "QR Code Modal closed");
+        });
+      }
+
+      const session = await approval();
+    } catch (e) {
+      console.log('error:')
+      console.error(e);
+      // ignore rejection
+    } finally {
+      // close modal in case it was open
+      QRCodeModal.close();
+    }
+  }, [client]);
 
   return (
     <div>
       <h1>Welcome</h1>
-      <button onClick={buttonClick}>Connect</button>
+      <button onClick={handleConnect}>Connect</button>
     </div>
   );
 }
