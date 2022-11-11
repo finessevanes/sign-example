32f376324768046f5a7c9c48fa6ea56059819fd9
Vanessa Mercado <finessevanes@Vanessas-MacBook-Pro.local>
2022-11-10 21:05:27 -0700

    No errors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { SignClient } from "@walletconnect/sign-client";
 
-function App() {
+function App({ buttonClick }) {
+  const handleConnect = () => {
+    console.log("beep boop");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome</h1>
+      <button onClick={buttonClick}>Connect</button>
     </div>
   );
 }
